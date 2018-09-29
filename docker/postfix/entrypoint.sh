297e6b08bea9c4786cafe81bd949f3e64cceafba
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-09-29 22:52:34 +0200

    initialize main.cf, master.cf in Dockerfile

--- docker/postfix/entrypoint.sh
+++ docker/postfix/entrypoint.sh
@@ -8,16 +8,4 @@ echo SMTPSERVER=${SMTPSERVER}
 echo SMTPUSERNAME=${SMTPUSERNAME}
 echo SMTPPASSWORD=${SMTPPASSWORD}
 
-echo postconf -e myhostname=$MAILDOMAIN
-echo postconf -F 'lmtp/*/chroot = n'
-
-if [ -n /etc/aliases ]; then
-	echo "no aliases"
-#	echo "root:${USERNAME}" >> /etc/aliases
-#	newaliases
-else
-	ls -l /etc/aliases*
-	cat /etc/aliases
-fi
-
 exec "$@"
