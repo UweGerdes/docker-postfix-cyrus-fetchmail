41d633634a1780b6835400c94149c0d6b0225ff0
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-02 22:59:13 +0200

    setup for cyrus and sasl

--- docker/mailserver/cyrususers.sh
+++ docker/mailserver/cyrususers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while IFS=":" read -r user pass; do
-	echo "saslpasswd2 create $user"
-	echo "$pass" | saslpasswd2 -c $user -p
+	echo "saslpasswd2 create $user ${MAILNAME}"
+	echo "$pass" | saslpasswd2 -c $user -p -u hostname
 done < "/home/cyrus/cyrususers"
