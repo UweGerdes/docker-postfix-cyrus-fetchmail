e937dae74c2fac9f9476db63171d24261677d746
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-04 23:33:30 +0200

    starting services

--- docker/mailserver/entrypoint.sh
+++ docker/mailserver/entrypoint.sh
@@ -5,7 +5,11 @@ if [ -n "$(find "/var/spool/postfix/" -maxdepth 0 -type d -empty 2>/dev/null)" ]
     cp -rp /var/spool/postfix.init/* /var/spool/postfix
 fi
 
-echo "postfix set-permissions"
 postfix set-permissions >/dev/null 2>&1
+
+service rsyslog start
+service saslauthd restart
+service postfix start
+service cyrus-imapd restart
 
 exec "$@"
