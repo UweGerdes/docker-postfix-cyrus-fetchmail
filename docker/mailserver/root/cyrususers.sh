4978f1beccc2dcaa0730ddc24eb505a13429e00c
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-04 21:52:02 +0200

    adding supervisor and rsyslog

--- docker/mailserver/root/cyrususers.sh
+++ docker/mailserver/root/cyrususers.sh
@@ -2,5 +2,5 @@
 
 while IFS=":" read -r user pass; do
 	echo "saslpasswd2 create $user"
-	echo "$pass" | saslpasswd2 -c $user -p -u ${MAILNAME}
+	echo "$pass" | saslpasswd2 -p -u ${MAILNAME} -c $user
 done < "/root/cyrususers"
