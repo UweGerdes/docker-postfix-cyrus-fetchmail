87d00baead73c819d0caacdf982bf6fbeac8e166
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-05 00:49:19 +0200

    create sasl users only on first run

--- docker/mailserver/entrypoint.sh
+++ docker/mailserver/entrypoint.sh
@@ -5,19 +5,20 @@ if [ -n "$(find "/var/spool/postfix/" -maxdepth 0 -type d -empty 2>/dev/null)" ]
 	cp -rp /var/spool/postfix.init/* /var/spool/postfix
 fi
 
-if [ -n "$(sasldblistusers2)" ]; then
+if [ -z "$(sasldblistusers2)" ]; then
 	while IFS=":" read -r user pass; do
 		echo "saslpasswd2 create $user"
 		echo "$pass" | saslpasswd2 -p -u ${MAILNAME} -c $user
 	done < "/root/cyrususers"
-    sasldblistusers2
+	sasldblistusers2
 fi
 
 postfix set-permissions >/dev/null 2>&1
 
+rm /run/rsyslogd.pid
 service rsyslog start
-service saslauthd restart
+service saslauthd start
 service postfix start
-service cyrus-imapd restart
+service cyrus-imapd start
 
 exec "$@"
