f592e83541d765acad64380e430ca27da9051b08
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-04 13:11:29 +0200

    changed folder for cyrususers

--- docker/mailserver/cyrususers.sh
+++ docker/mailserver/cyrususers.sh
@@ -3,4 +3,4 @@
 while IFS=":" read -r user pass; do
 	echo "saslpasswd2 create $user"
 	echo "$pass" | saslpasswd2 -c $user -p -u ${MAILNAME}
-done < "/home/cyrus/cyrususers"
+done < "/root/cyrususers"
