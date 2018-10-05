279f7f9a66fa2a669df826e169107921a89c66d6
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-05 23:04:02 +0200

    refactoring fetchstart.sh

--- var/lib/fetchmail/fetchstart.sh
+++ var/lib/fetchmail/fetchstart.sh
@@ -5,21 +5,21 @@ HOSTNAME=`hostname`
 MYNAME=`whoami`
 LOGFILE="/var/log/fetchmail.log"
 
-echo -n "${DATE} ${HOSTNAME} fetchstart" >> ${LOGFILE}
+echo -n "${DATE} ${HOSTNAME}" >> ${LOGFILE}
 
 if [ -f "/tmp/fetchstart.lock" ]; then
-	echo " - mailserver locked - quit." >> ${LOGFILE}
+	echo " - server locked - quit." >> ${LOGFILE}
 	exit 0
 fi
 
 if [ -z "`/usr/bin/pgrep cyrmaster`" ]; then
 	echo " - cyrus-imapd not running - quit." >> ${LOGFILE}
+	exit 0
+fi
+
+if [ -z "`/usr/bin/pgrep fetchmail`" ] ; then
+	echo ", fetchmail starting." >> ${LOGFILE}
+	/usr/bin/fetchmail -f /var/lib/fetchmail/fetchmailrc --timeout 30 >> ${LOGFILE} 2>&1
 else
-	if [ -z "`/usr/bin/pgrep fetchmail`" ] ; then
-		echo ", fetchmail starting." >> ${LOGFILE}
-		echo "${DATE} ${HOSTNAME} ${MYNAME} starting" >> /var/log/fetchmail.log
-		/usr/bin/fetchmail -f /var/lib/fetchmail/fetchmailrc --timeout 30 >> /var/log/fetchmail.log 2>&1
-	else
-		echo ", fetchmail running, PID is `/usr/bin/pgrep fetchmail` - quit." >> ${LOGFILE}
-	fi
+	echo ", fetchmail running, PID is `/usr/bin/pgrep fetchmail` - quit." >> ${LOGFILE}
 fi
