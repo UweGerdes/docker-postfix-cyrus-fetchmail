74cd4cfc45c90c1c05ca6f4dfe5167bc953687c6
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-28 12:01:14 +0100

    added login check for targethost, optimized script

---
+++ usr/local/bin/check_targethost.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+TARGETHOST="$1"
+
+if [ -z "${TARGETHOST}" ] ; then
+	echo "${0} missing argument: targethost"
+	exit 1
+fi
+
+echo "Try connection to ${TARGETHOST}"
+
+LOCAL_TIME=`date +%s`
+REMOTE_TIME=`sudo -u cyrus ssh -p 61022 cyrus@${0} date +%s`
+
+if [ -z "${REMOTE_TIME}" ] ; then
+	read -p "${TARGETHOST} host not found - exiting"
+	exit 2
+fi
+
+DIFF_TIME=$((REMOTE_TIME-LOCAL_TIME))
+echo "local: ${LOCAL_TIME}, remote: ${REMOTE_TIME}, diff: ${DIFF_TIME}"
+
+if [ "${DIFF_TIME}" > 5 ] ; then
+	read -p "install key based login to ${TARGETHOST}? [RETURN]"
+	sudo -H -u cyrus sh -c "ssh-keygen -t rsa -C cyrus@mailserver -N '' -f ~/.ssh/id_rsa && ssh-copy-id -i ~/.ssh/id_rsa.pub -p 61022 cyrus@${TARGETHOST}"
+fi
--- usr/local/bin/cyrus_rsync.sh
+++ usr/local/bin/cyrus_rsync.sh
@@ -1,51 +1,39 @@
 #!/bin/bash
 
-TARGETHOST="$1"
-
-if [ -z "${TARGETHOST}" ] ; then
-	echo "${0} missing argument: targethost"
-	exit 1
-fi
-
 if [ "`whoami`" != "root" ] ; then
 	echo "${0} has to be started as user root"
 	exit 1
 fi
 
-if [ ! -f "/root/rsyncusers.${TARGETHOST}" ] ; then
-	echo "${0} missing file: /root/rsyncusers.${TARGETHOST} - are you on a replicated system?"
-	exit 1
+TARGETHOST="$1"
+
+if [ -z "${TARGETHOST}" ] ; then
+	echo "${0} missing argument: targethost"
+	exit 2
 fi
 
-echo "Try connection to ${TARGETHOST}"
-sudo -u cyrus ssh -p 61022 cyrus@${TARGETHOST} echo "connection established"
+if [ ! -f "/root/rsyncusers.${TARGETHOST}" ] ; then
+	echo "${0} missing file: /root/rsyncusers.${TARGETHOST} - are you on a replicated system?"
+	exit 3
+fi
+
+/usr/local/bin/check_targethost.sh ${TARGETHOST}
+if [ $? > 0 ] ; then
+	echo "${0} connection to ${TARGETHOST} not established"
+	exit 4
+fi
 
 /usr/local/bin/mailserverstop.sh
 
 FETCHPID=`/usr/bin/pgrep fetchmail`
 if [ -z "${FETCHPID}" ] ; then
 	sudo -u cyrus ssh -p 61022 cyrus@${TARGETHOST} sudo /usr/local/bin/mailserverstop.sh
-	#sleep 10
-	MAILSERVERRUN=`/bin/ps ax | egrep 'postfix/sbin/.?master|cyr.?master'`
-	if [ -n "${MAILSERVERRUN}" ] ; then
-		echo "stopping mailserver - please wait"
-		/usr/sbin/postfix stop
-		sleep 2
-		service cyrus-imapd stop
-		sleep 2
-	else
-		echo "kein Stop des Mail-Systems"
-	fi
 
 	# cyrus lib files
 	sudo -u cyrus /usr/bin/rsync -e "ssh -p 61022 -l cyrus" --delete -rtpvogzi --size-only "/var/lib/cyrus/" "${TARGETHOST}:/var/lib/cyrus"
 
-	# for all users in replication list
-	while IFS=" " read -r user ; do
-		if [ -d "/var/spool/cyrus/mail/${user:0:1}/user/${user}" ]; then
-			echo ""
-			echo "rsync mailbox for $user"
-			sudo -u cyrus /usr/bin/rsync -e "ssh -p 61022 -l cyrus" --delete -rtpvogzi "/var/spool/cyrus/mail/${user:0:1}/user/${user}/" "${TARGETHOST}:/var/spool/cyrus/mail/${user:0:1}/user/${user}"
+	# cyrus mail files
+	sudo -u cyrus /usr/bin/rsync -e "ssh -p 61022 -l cyrus" --delete -rtpvogzi "/var/spool/cyrus/mail/" "${TARGETHOST}:/var/spool/cyrus/mail"
 		fi
 	done < "/root/rsyncusers.${TARGETHOST}"
 
