8486bc203718b20d454c70b2e58f5eedc2d16e67
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-10-28 12:09:33 +0100

    updated checks

--- root/post-hook.sh
+++ root/post-hook.sh
@@ -3,11 +3,17 @@
 CERTBOTFILE="/root/.certbot"
 
 if [ ! -f "$CERTBOTFILE" ]; then
-	echo "File $CERTBOTFILE not found. Please place .certbot file in appropriate location."
-	exit 2
+	echo "File $CERTBOTFILE not found. Please add  \"CERTBOT_DOMAIN=your.domain.com\" to that file."
+	exit 1
 fi
 
 source $CERTBOTFILE
+
+if [ -z "$CERTBOT_DOMAIN" ]; then
+	echo "Please add  \"CERTBOT_DOMAIN=your.domain.com\" to $CERTBOTFILE."
+	echo "Please execute 'chgrp -R ssl-cert /etc/letsencrypt/live /etc/letsencrypt/archive'"
+	exit 2
+fi
 
 if [ "$(ls -l /etc/letsencrypt/live/$CERTBOT_DOMAIN/cert.pem | awk '{print $4}')" != "ssl-cert" ] ; then
 	echo "$(date -u +'%b %d %H:%M:%S') $0 access rights for letencrypt need update" | tee /var/log/mailserver.err
